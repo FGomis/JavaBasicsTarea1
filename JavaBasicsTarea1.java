a1bc921cb4fd211fac677fbb433f46db8b21b424
FGomis <fkgomis@gmail.com>
2021-07-07 11:07:39 +0200

    Create JavaBasicsTarea1.java

---
+++ JavaBasicsTarea1.java
@@ -0,0 +1,26 @@
+
+public class JavaBasicsTarea1 {
+
+	public static void main(String[] args) {
+		
+		int a = 30;
+		int b = 5;
+		int r;
+		
+		r=a+b;
+		System.out.println("La suma de "+a+" y "+b+" da "+r);
+		
+		r=a-b;
+		System.out.println("La resta "+a+" menos "+b+" da "+r);
+		
+		r=a*b;
+		System.out.println("La multiplicación de "+a+" y "+b+" da "+r);
+		
+		r=a/b;
+		System.out.println("La división de "+a+" entre "+b+" da "+r);
+		
+		r=a%b;
+		System.out.println("El resto de la división entre "+a+" y "+b+" es "+r);
+	}
+
+}
